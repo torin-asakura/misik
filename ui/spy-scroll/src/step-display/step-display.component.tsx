3b479228927bf7244fe097a377ef83caaea98d38
Kirill <kirillzoloto55@gmail.com>
2022-03-23 13:21:41 +0400

    feat(spy-scroll): added data

--- ui/spy-scroll/src/step-display/step-display.component.tsx
+++ ui/spy-scroll/src/step-display/step-display.component.tsx
@@ -1,5 +1,6 @@
 import React           from 'react'
 import { FC }          from 'react'
+import { useState }    from 'react'
 
 import styled          from '@emotion/styled'
 
@@ -9,6 +10,7 @@ import { Layout }      from '@ui/layout'
 import { Column }      from '@ui/layout'
 import { Condition }   from '@ui/condition'
 import { useLanguage } from '@globals/language'
+import { useSteps }    from '../data'
 
 const VerticalText: FC = styled(Text)({
   transform: 'rotate(-90deg)',
@@ -19,46 +21,38 @@ const TransitionBox = styled(Box)({
   transition: '.2s',
 })
 
-const StepDisplay = ({ activeDot }) => (
-  <Column fill alignItems='center'>
-    <VerticalText>
-      <Condition match={activeDot === 0}>
-        MisikLaw
-      </Condition>
-      <Condition match={activeDot === 1}>
-        О нас
-      </Condition>
-      <Condition match={activeDot === 2}>
-        Услуги
-      </Condition>
-      <Condition match={activeDot === 3}>
-        Формат работы
-      </Condition>
-      <Condition match={activeDot === 4}>
-        Обратная связь
-      </Condition>
-    </VerticalText>
-    <Layout flexGrow={1} flexBasis={32} />
-    <Box backgroundColor='background.lightGray' width={2} height={240}>
-        <Box width={48}>
-          <Condition match={activeDot === 0}>
-            <TransitionBox height={48} backgroundColor='black' width='100%' />
-          </Condition>
-          <Condition match={activeDot === 1}>
-            <TransitionBox height={96} backgroundColor='black' width='100%' />
-          </Condition>
-          <Condition match={activeDot === 2}>
-            <TransitionBox height={144} backgroundColor='black' width='100%' />
-          </Condition>
-          <Condition match={activeDot === 3}>
-            <TransitionBox height={192} backgroundColor='black' width='100%' />
-          </Condition>
-          <Condition match={activeDot === 4}>
-            <TransitionBox height={240} backgroundColor='black' width='100%' />
-          </Condition>
-        </Box>
-    </Box>
-  </Column>
-)
+const StepDisplay = ({ activeDot }) => {
+  const [step, setStep] = useState<string>('')
+  const steps = useSteps()
+  const [language] = useLanguage()
+
+  return (
+    <Column fill alignItems='center'>
+      <VerticalText>
+        {steps[language].find((s) => s.stepAddons.stepid === step)}
+      </VerticalText>
+      <Layout flexGrow={1} flexBasis={32} />
+      <Box backgroundColor='background.lightGray' width={2} height={240}>
+          <Box width={48}>
+            <Condition match={activeDot === 0}>
+              <TransitionBox height={48} backgroundColor='black' width='100%' />
+            </Condition>
+            <Condition match={activeDot === 1}>
+              <TransitionBox height={96} backgroundColor='black' width='100%' />
+            </Condition>
+            <Condition match={activeDot === 2}>
+              <TransitionBox height={144} backgroundColor='black' width='100%' />
+            </Condition>
+            <Condition match={activeDot === 3}>
+              <TransitionBox height={192} backgroundColor='black' width='100%' />
+            </Condition>
+            <Condition match={activeDot === 4}>
+              <TransitionBox height={240} backgroundColor='black' width='100%' />
+            </Condition>
+          </Box>
+      </Box>
+    </Column>
+  )
+}
 
 export { StepDisplay }
