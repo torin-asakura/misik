c3d41a2df820c4d45b99a4887d9f06f97217c4c1
Kirill <kirillzoloto55@gmail.com>
2022-03-26 19:11:10 +0400

    fix(index-page): navigation color

--- landing/index-page/src/index.page.tsx
+++ landing/index-page/src/index.page.tsx
@@ -1,24 +1,26 @@
-import React                               from 'react'
-import { FC }                              from 'react'
-import { useState }                        from 'react'
+import React                       from 'react'
+import { FC }                      from 'react'
+import { useState }                from 'react'
 
-import { Navigation }                      from '@landing/navigation-fragment'
-import { Hero }                            from '@landing/hero-fragment'
-import { About }                           from '@landing/about-fragment'
-import { Services }                        from '@landing/services-fragment'
-import { WorkFormat }                      from '@landing/work-format-fragment'
-import { WorkDirections }                  from '@landing/work-directions-fragment'
-import { Feedback }                        from '@landing/feedback-fragment'
-import { Map }                             from '@landing/map-fragment'
-import { Footer }                          from '@landing/footer-fragment'
-import { DataProvider }                    from '@globals/data'
-import { LanguageProvider }                from '@globals/language'
-import { Language }                        from '@globals/language'
-import { SpyScroll }                       from '@ui/spy-scroll'
-import { Drawer }                          from '@ui/drawer'
-import { useIntersectionObserver }         from '@ui/intersection-observer'
+import { Navigation }              from '@landing/navigation-fragment'
+import { Hero }                    from '@landing/hero-fragment'
+import { About }                   from '@landing/about-fragment'
+import { Services }                from '@landing/services-fragment'
+import { WorkFormat }              from '@landing/work-format-fragment'
+import { WorkDirections }          from '@landing/work-directions-fragment'
+import { Feedback }                from '@landing/feedback-fragment'
+import { Map }                     from '@landing/map-fragment'
+import { Footer }                  from '@landing/footer-fragment'
+import { DataProvider }            from '@globals/data'
+import { LanguageProvider }        from '@globals/language'
+import { Language }                from '@globals/language'
+import { SpyScroll }               from '@ui/spy-scroll'
+import { Drawer }                  from '@ui/drawer'
+import { useIntersectionObserver } from '@ui/intersection-observer'
 
-import { Seo }                             from './seo.component'
+import { Seo }                     from './seo.component'
+
+let isCalled = false
 
 const IndexPage: FC = () => {
   const languageContext = useState<Language>('RU')
@@ -28,7 +30,14 @@ const IndexPage: FC = () => {
   const { getObserverOptions } = useIntersectionObserver((id) => {
     const order = ['hero', 'about', 'services', 'work-format', 'feedback']
 
-    setActive(order.indexOf(id))
+    if (isCalled) {
+      setActive(order.indexOf(id))
+    }
+     else {
+       setTimeout(() => {
+         isCalled = true
+       }, 200)
+    }
   })
 
   return (
