ea32cf026fdc32abd7e14241d011246ef1e8082f
Nikita Elfimov <me@tfk.name>
2022-07-04 21:29:09 +0500

    chore(app): reinit with icons

--- landing/app/src/pages/_document.tsx
+++ landing/app/src/pages/_document.tsx
@@ -1,9 +1,35 @@
 import { withHelmet }    from '@atls/next-document-with-helmet'
-import { withIcons }     from '@atls/next-document-with-icons'
 import { withOpenGraph } from '@atls/next-document-with-opengraph'
 
 import Document          from 'next/document'
+import React             from 'react'
 import compose           from 'recompose/compose'
+
+const withIcons = () => (TargetComponent) =>
+  class WithIcons extends TargetComponent {
+    static async getInitialProps(context) {
+      const props = await super.getInitialProps(context)
+
+      props.head.push(
+        <link
+          rel='apple-touch-icon'
+          href='https://wp.misik.pro/wp-content/uploads/2022/07/apple_touch_icon.png'
+        />,
+        <link
+          rel='icon'
+          type='image/png'
+          href='https://wp.misik.pro/wp-content/uploads/2022/07/apple_touch_icon.png'
+        />
+      )
+
+      return props
+    }
+
+    static renderDocument(...args) {
+      // @ts-ignore
+      return Document.renderDocument(...args)
+    }
+  }
 
 const withProviders = compose(
   withOpenGraph({ image: 'https://misik.pro/wp-content/uploads/2021/06/Misik-Dark.svg' }),
