4ba33125779221d48989bd3d912ec170f0c48421
Kirill Zolottsev <carmine@atls.team>
2022-07-21 16:12:38 +0400

    fix(relocation-grounds-status-fragment): layout

--- landing/relocation-grounds-status-fragment/src/relocation-grounds-status.component.tsx
+++ landing/relocation-grounds-status-fragment/src/relocation-grounds-status.component.tsx
@@ -47,46 +47,49 @@ const RelocationGroundsStatus: FC = () => {
             <Layout flexBasis={[32, 32, 64]} flexShrink={0} />
             <Row flexWrap='wrap'>
               {groundsStatus.map(({ id, title, description }, index) => (
-                <Column key={id} width={['100%', '100%', 600]} height={[262, 262, 319]}>
-                  <Row>
-                    <Text
-                      fontSize={['big', 'big', 'semiGiant']}
-                      lineHeight='small'
-                      fontFamily='secondary'
-                      color='text.accent'
-                      fontStyle='italic'
-                      style={{ fontVariantNumeric: 'lining-nums' }}
-                    >
-                      {`0${index + 1}`}
-                    </Text>
-                  </Row>
-                  <Layout flexBasis={24} />
-                  <Row>
-                    <Text
-                      color='text.primary'
-                      fontSize={['semiLarge', 'semiLarge', 'enlarged']}
-                      lineHeight='small'
-                      fontFamily='secondary'
-                      textTransform='uppercase'
-                      fontWeight={['medium', 'medium', 'normal']}
-                    >
-                      {title}
-                    </Text>
-                  </Row>
-                  <Layout flexBasis={16} />
-                  <Row width={['100%', '100%', 540]}>
-                    <Text
-                      color='text.secondary'
-                      fontSize={['tiny', 'tiny', 'regular']}
-                      lineHeight={['big', 'big', 'small']}
-                    >
-                      {description}
-                    </Text>
-                  </Row>
+                <Column height='auto'>
+                  <Column key={id} width={['100%', '100%', 600]} height={[262, 262, 319]}>
+                    <Row>
+                      <Text
+                        fontSize={['big', 'big', 'semiGiant']}
+                        lineHeight='small'
+                        fontFamily='secondary'
+                        color='text.accent'
+                        fontStyle='italic'
+                        style={{ fontVariantNumeric: 'lining-nums' }}
+                      >
+                        {`0${index + 1}`}
+                      </Text>
+                    </Row>
+                    <Layout flexBasis={24} />
+                    <Row>
+                      <Text
+                        color='text.primary'
+                        fontSize={['semiLarge', 'semiLarge', 'enlarged']}
+                        lineHeight='small'
+                        fontFamily='secondary'
+                        textTransform='uppercase'
+                        fontWeight={['medium', 'medium', 'normal']}
+                      >
+                        {title}
+                      </Text>
+                    </Row>
+                    <Layout flexBasis={16} />
+                    <Row width={['100%', '100%', 540]}>
+                      <Text
+                        color='text.secondary'
+                        fontSize={['tiny', 'tiny', 'regular']}
+                        lineHeight={['big', 'big', 'small']}
+                      >
+                        {description}
+                      </Text>
+                    </Row>
+                  </Column>
+                  <Layout flexBasis={[40, 40, 0]} flexShrink={0} />
                 </Column>
               ))}
             </Row>
-            <Layout flexBasis={[64, 64, 160]} flexShrink={0} />
+            <Layout flexBasis={[24, 24, 160]} flexShrink={0} />
           </Column>
         </Layout>
       </AnimateOnLoad>
