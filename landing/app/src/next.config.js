da8fb2658a5b9852a9cd7f8bec1c00ffa71c4936
Dmitry <dimafomin02@gmail.com>
2024-01-30 23:51:06 +0300

    chore: update next config

--- landing/app/src/next.config.js
+++ landing/app/src/next.config.js
@@ -10,4 +10,24 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   staticPageGenerationTimeout: 1000,
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(gltf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/media/[name][ext]',
+        },
+      },
+      {
+        test: /\.(bin)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/media/[name][ext]',
+        },
+      }
+    )
+
+    return config
+  },
 }
