8884d4c0bee196fc59667e21c565ef6dac47370d
Dmitry <dimafomin02@gmail.com>
2024-01-31 22:30:44 +0300

    feat: added canvas inner

---
+++ ui/3d-elements/src/canvas-inner/canvas-inner.component.tsx
@@ -0,0 +1,23 @@
+import { Canvas }           from '@react-three/fiber'
+
+import React                from 'react'
+import { memo }             from 'react'
+
+import { CanvasInnerProps } from './canvas-inner.interfaces'
+
+export const CanvasInner = memo<CanvasInnerProps>(({ style, children }) => (
+  <Canvas
+    style={{
+      ...style,
+      background: 'radial-gradient(246.26% 141.42% at 0% 0%, #53535A 0%, #1C1C20 59.72%)',
+      borderTopLeftRadius: 40,
+      borderTopRightRadius: 40,
+    }}
+    camera={{ position: [10, 0, 0], fov: 50 }}
+  >
+    <ambientLight color='#ffffff' intensity={0.3} />
+    <directionalLight color='#ffffff' position={[20, 20, 0]} intensity={5.5} />
+    <directionalLight color='#ffffff' position={[10, -10, -10]} intensity={0.8} />
+    {children}
+  </Canvas>
+))
